f688cff9f33e1fc6739f60ae0836ad92c8b5dad4
吴文琪 <wuwenqi@wuwenqideMacBook-Pro.local>
2023-10-04 03:54:06 -0700

    change

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "hirebeatinterntry48b7c4be": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
